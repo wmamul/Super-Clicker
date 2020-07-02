c9046be603e73f47244e4dea51cad553612d9215
wmamul <wojciech.mamul@gmail.com>
2020-07-03 00:51:19 +0200

    Implemented session context manager -- #TODO: refractor db interface using scoped_session()

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -1,3 +1,19 @@
+from contextlib import contextmanager
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.exceptions import DatabaseError
 
-engine = create_engine('
+engine = create_engine('sqlite:///../db.sqlite3', echo=True)
+Session = sessionmaker(bind=engine)
+
+@contextmanager
+def session_scope():
+    session = Session()
+    try:
+        yield session
+        session.commit()
+    except:
+        session.rollback()
+        raise DatabaseError('Commiting session raised an exception')
+    finally:
+        session.close()
