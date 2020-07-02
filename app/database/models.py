9fca61fe122425124ded9746f443e80f5ab5cc9b
wmamul <wojciech.mamul@gmail.com>
2020-07-03 00:27:28 +0200

    Initial refractoring for raw sqlalchemy. #TODO: Implement db engine and session. Interface module needs to be refractored, as well as relationships between models. v2.0

--- app/database/models.py
+++ app/database/models.py
@@ -1,3 +1,4 @@
+from typing import Dict
 from datetime import datetime, timedelta
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Sequence
@@ -13,26 +14,26 @@ class User(Base, UserMixin):
     __tablename__ = 'users'
 
     id = Column(String(36), Sequence('user_id_seq'), default=lambda:
-            str(uuid.uuid4()), unique=True, primary_key=True)
+         str(uuid.uuid4()), unique=True, primary_key=True)
     username = Column(String(20), unique=True, nullable=False)
     email = Column(String(120), unique=True, nullable=False)
     image = Column(String(100), nullable=False,
             default='../../media/default_image.jpg')
     last_login = Column(DateTime, unique=False, nullable=False,
-            default=datetime.utcnow())
+                 default=datetime.utcnow())
     password_hash = Column(String(60), nullable=False)
 #    token = relationship('Token', backref='user', lazy=True)
 #    progress = relationship('Progress', backref='user', lazy=True)
 
-    def __repr__(self):
-        user = (self.username, self.email, self.image, self.id)
-        print(f"User: {user}")
+    def __repr__(self) -> str:
+        return '<User(username=%s, email=%s, image=%s, last_login=%s, password_hash=%s)>' % (
+                self.username, self.email, self.image, self.last_login, self.password_hash)
 
-    def info(self):
-        user = (("username", self.username),
-                ("email", self.email),
-                ("image", self.image),
-                ("id", self.id))
+    def info(self) -> Dict:
+        user = (('username', self.username),
+                ('email', self.email),
+                ('image', self.image),
+                ('id', self.id))
         return dict(user)
 
 class Token(Base):
@@ -40,21 +41,21 @@ class Token(Base):
     __tablename__ = 'tokens'
 
     id = Column(String(36), Sequence('token_id_seq'), default=lambda:
-            str(uuid.uuid4()), unique=True, primary_key=True)
+         str(uuid.uuid4()), unique=True, primary_key=True)
     user_id = Column(String(36), ForeignKey('user.id'), nullable=False)
     timestamp = Column(DateTime, default=datetime.utcnow())
     expiry = Column(DateTime, default=datetime.utcnow() +
-            timedelta(seconds=EXP_TIME))
+             timedelta(seconds=EXP_TIME))
 
-    def __init__(self, user):
+    def __init__(self, user: User):
         self.user_id = user.id
 
-    def is_valid(self):
+    def is_valid(self) -> bool:
         if self.expiry > datetime.utcnow():
             return True
         return False
 
-    def refresh(self):
+    def refresh(self) -> None:
         self.expiry = datetime.utcnow() + timedelta(seconds=EXP_TIME)
 '''
 #TODO: Define progress table
