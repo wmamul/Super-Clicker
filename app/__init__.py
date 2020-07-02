c83bb089a013a4d1759078f5e12f2302085f497b
wmamul <wojciech.mamul@gmail.com>
2020-07-03 00:51:50 +0200

    Implemented session context manager -- #TODO: refractor db interface using scoped_session()

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
 import app.api 
 from app.api import resources
-import app.database 
 import app.users 
 from app.users import auth
 
@@ -10,6 +9,5 @@ app.config['SECRET_KEY'] = '3Ri5/uVHnSzpIg0Zn2cSDuPkwDp9skUy1RClPzevYS8='
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 
 api.api.init_app(app)
-database.db.init_app(app)
 users.login_manager.init_app(app)
 users.bcrypt.init_app(app)
